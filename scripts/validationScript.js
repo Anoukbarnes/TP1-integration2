817c35147e1c37da34251b74c27df9759333ddd2
Anoukbarnes <anouk721@gmail.com>
2024-05-25 15:03:25 -0400

    JS form validation

--- scripts/validationScript.js
+++ scripts/validationScript.js
@@ -1,73 +1,58 @@
+const form = document.getElementById('form');
 const firstName = document.getElementById('first-name');
 const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
-const txtInfo = document.getElementById('txt-info');
 
-const validateForm = () => {
-    let noError = true;
-    const firstNameValue = firstName.value.trim();
-    const lastNameValue = lastName.value.trim();
-    const emailValue = email.value.trim();
-    const txtInfoValue = txtInfo.value.trim();
 
-    
 
-    if (firstNameValue === '') {
-        setError(firstName, 'Veuillez entrer un prénom');
-        noError = false;
-    }else {
-        setSuccess(firstName);
-    }
+//Show input error message 
+function showError(input, message) {
+    const inputControl = input.parentElement;
+    inputControl.classList.add('error');
+    const errorMessage = inputControl.querySelector('.errorMessage');
+    errorMessage.innerText = message;
+    console.log(message);
+}
 
-    if (lastNameValue === '') {
-        setError(lastName, 'Veuillez entrer un nom');
-        noError = false;
-    }else {
-        setSuccess(lastName);
-    }
-
-    if (txtInfoValue === '') {
-        setError(txtInfo, 'Veuillez entrer un message');
-        noError = false;
-    }else {
-        setSuccess(txtInfo);
-    }
-
-    if (!checkEmail(emailValue)) {
-        setError(email, 'Veuillez entrer un courriel valide');
-        noError = false;
-    } else {
-        setSuccess(email);
-    }
-
-    return noError;
+//Show success outline
+function showSuccess(input) {
+    const inputControl = input.parentElement;
+    inputControl.classList.remove('error');
+    inputControl.classList.add('success');
+    const errorMessage = inputControl.querySelector('.errorMessage');
+    errorMessage.innerText = '';
 }
 
 
-//Vérification courriel
 function checkEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
-//Message d'erreur
-const setError = (input, message) => {
-    const inputControl = input.parentElement;
-    const errorDisplay = inputControl.querySelector('.form__input--errorMessage');
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('--error');
-    inputControl.classList.remove('--success');
-}
+//Event Listeners
+form.addEventListener('submit', function(e){
+    e.preventDefault();
 
-//Message de succès
-const setSuccess = input => {
-    const inputControl = input.parentElement;
-    console.log("le input control de Set Success est: " + inputControl);
-    const errorDisplay = inputControl.querySelector('.form__input--errorMessage');
+    if(firstName.value === '') {
+        showError(firstName, 'Veillez entrer un prénom')
+    } else {
+        showSuccess(firstName);
+    }
 
-    errorDisplay.innerText = '';
-    inputControl.classList.remove('--error');
-    inputControl.classList.add('--success');
-}
+    if(lastName.value === '') {
+        showError(lastName, 'Veillez entrer un nom')
+    } else {
+        showSuccess(lastName);
+    }
+
+
+    if (!checkEmail(email.value)) {
+        showError(email, 'Veuillez entrer un courriel valide');
+    } else {
+        showSuccess(email);
+    }
+
+});
+
 
